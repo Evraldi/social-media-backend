a47e7cb414dd7adac469037e92ef993ebd64b9f9
Evraldi <pekkapower9090@gmail.com>
2024-08-01 06:25:24 +0700

    updated relationship and models

--- models/index.js
+++ models/index.js
@@ -3,22 +3,48 @@ const User = require('./user');
 const Post = require('./post');
 const Comment = require('./comment');
 const Like = require('./like');
+const Friendship = require('./friendship');
+const Message = require('./message');
+const Follower = require('./follower');
+const Media = require('./media');
+const Notification = require('./notification');
 
+// User Relationships
 User.hasMany(Post, { foreignKey: 'user_id' });
 Post.belongsTo(User, { foreignKey: 'user_id' });
-
-Post.hasMany(Comment, { foreignKey: 'post_id' });
-Comment.belongsTo(Post, { foreignKey: 'post_id' });
 
 User.hasMany(Comment, { foreignKey: 'user_id' });
 Comment.belongsTo(User, { foreignKey: 'user_id' });
 
-Post.hasMany(Like, { foreignKey: 'post_id' });
-Like.belongsTo(Post, { foreignKey: 'post_id' });
-
 User.hasMany(Like, { foreignKey: 'user_id' });
 Like.belongsTo(User, { foreignKey: 'user_id' });
 
+User.hasMany(Friendship, { foreignKey: 'requester_id', as: 'SentFriendRequests' });
+User.hasMany(Friendship, { foreignKey: 'receiver_id', as: 'ReceivedFriendRequests' });
+
+User.hasMany(Message, { foreignKey: 'sender_id', as: 'Sender' });
+User.hasMany(Message, { foreignKey: 'receiver_id', as: 'Receiver' });
+
+User.hasMany(Follower, { foreignKey: 'follower_id', as: 'Follower' });
+User.hasMany(Follower, { foreignKey: 'following_id', as: 'Following' });
+
+User.hasMany(Media, { foreignKey: 'user_id' });
+Media.belongsTo(User, { foreignKey: 'user_id' });
+
+User.hasMany(Notification, { foreignKey: 'user_id' });
+Notification.belongsTo(User, { foreignKey: 'user_id' });
+
+// Post Relationships
+Post.hasMany(Comment, { foreignKey: 'post_id' });
+Comment.belongsTo(Post, { foreignKey: 'post_id' });
+
+Post.hasMany(Like, { foreignKey: 'post_id' });
+Like.belongsTo(Post, { foreignKey: 'post_id' });
+
+Post.hasMany(Media, { foreignKey: 'post_id' });
+Media.belongsTo(Post, { foreignKey: 'post_id' });
+
+// Sync DB
 const initDb = async () => {
     try {
         await sequelize.sync({ force: false });
@@ -28,4 +54,4 @@ const initDb = async () => {
     }
 };
 
-module.exports = { sequelize, User, Post, Comment, Like, initDb };
+module.exports = { sequelize, User, Post, Comment, Like, Friendship, Message, Follower, Media, Notification, initDb };

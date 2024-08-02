3dc906c0d863f8edd783253897f5fa2ccbc2c1d2
Evraldi <pekkapower9090@gmail.com>
2024-08-03 06:30:03 +0700

    add more features in message

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const { sendMessage, getMessages } = require('../controllers/messageController');
+const { sendMessage, getMessages, updateMessage, updateMessageStatus, deleteMessage, getConversation } = require('../controllers/messageController');
 const router = express.Router();
 
 router.post('/', sendMessage);
 router.get('/:sender_id/:receiver_id', getMessages);
+router.get('/conversation/:sender_id/:receiver_id', getConversation);
+router.put('/:id', updateMessage);
+router.put('/status/:id', updateMessageStatus);
+router.delete('/:id', deleteMessage);
 
 module.exports = router;

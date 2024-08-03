05bdf8a794c2c986a618b3bc8d3fe1eb31625aab
Evraldi <pekkapower9090@gmail.com>
2024-08-04 06:23:19 +0700

    changed in variable name

--- routes/likeRoutes.js
+++ routes/likeRoutes.js
@@ -4,6 +4,6 @@ const router = express.Router();
 
 router.post('/like', likePost);
 router.post('/unlike', unlikePost);
-router.get('/likes/:postId', getLikesByPostId);
+router.get('/likes/:post_id', getLikesByPostId);
 
 module.exports = router;
